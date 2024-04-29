0ebcefd0d0be22689258891970c17c5bca77de47
Fran <franobertozarazaga@gmail.com>
2024-04-29 01:10:31 -0300

    [times] Times

---
+++ times.py
@@ -0,0 +1,70 @@
+data = '0x (ZRX)	Aproximadamente 70 confirmaciones	14 minutos 1inch	Aproximadamente 70 confirmaciones	14 minutos Aave (AAVE)	Aproximadamente 70 confirmaciones	14 minutos Aavegotchi (GHST)	Aproximadamente 70 confirmaciones	14 minutos Acala (ACA)	N/A	Casi al instante Adventure Gold (AGLD)	Aproximadamente 70 confirmaciones	14 minutos Akash (AKT)	N/A	Casi al instante Alchemix (ALCX)	Aproximadamente 70 confirmaciones 14 minutos Alchemy Pay (ACH)	Aproximadamente 70 confirmaciones 14 minutos Algorand (ALGO)	10 confirmaciones	45 segundos Alien Worlds (TLM)	Aproximadamente 70 confirmaciones	14 minutos Alpha Venture DAO (ALPHA)	Aproximadamente 70 confirmaciones	14 minutos Altair (AIR)	N/A	Casi al instante Ambire AdEx (ADX)	Aproximadamente 70 confirmaciones	14 minutos Ampleforth Governance Token (FORTH)	Aproximadamente 70 confirmaciones	14 minutos Ankr (ANKR)	Aproximadamente 70 confirmaciones	14 minutos ApeCoin (APE)	Aproximadamente 70 confirmaciones	14 minutos API3 (API3)	Aproximadamente 70 confirmaciones	14 minutos Aptos (APT)	50 confirmaciones	5 minutos Aragon (ANT)	Aproximadamente 70 confirmaciones	14 minutos Arpa Chain (ARPA)	Aproximadamente 70 confirmaciones	14 minutos Astar (ASTR)	25 confirmaciones	2 minutos Audius (AUDIO)	Aproximadamente 70 confirmaciones	14 minutos Augur (REP)	Aproximadamente 70 confirmaciones	14 minutos Augur v2 (REPV2)	Aproximadamente 70 confirmaciones	14 minutos Avalanche (AVAX)	20 confirmaciones	1 minuto Axie Infinity (AXS)	Aproximadamente 70 confirmaciones	14 minutos Badger DAO (BADGER)	Aproximadamente 70 confirmaciones	14 minutos Balancer (BAL)	Aproximadamente 70 confirmaciones	14 minutos Bancor (BNT)	Aproximadamente 70 confirmaciones	14 minutos Band Protocol (BAND)	Aproximadamente 70 confirmaciones	14 minutos Barnbridge (BOND)	Aproximadamente 70 confirmaciones 14 minutos Basic Attention Token (BAT)	Aproximadamente 70 confirmaciones	14 minutos Basilisk (BSX)	N/A	Casi al instante Biconomy (BICO)	Aproximadamente 70 confirmaciones	14 minutos Bifrost (BNC)	N/A	Casi al instante Bitcoin (BTC)	3 confirmaciones	 EST. 40 minutos Depende de la tarifa Bitcoin (BTC Lightning Network)	N/A	Casi al instante Bitcoin Cash (BCH)	15 confirmaciones	2,5 horas (150 minutos) BitDAO (BIT)	Aproximadamente 70 confirmaciones	14 minutos Bittorrent (BTT)	20 confirmaciones	2 minutos Bluzelle (BLZ)	Aproximadamente 70 confirmaciones	14 minutos Boba Network (BOBA)	Aproximadamente 70 confirmaciones	14 minutos Bonfida (FIDA)	N/A	Casi al instante Cardano (ADA)	15 confirmaciones	10 minutos Cartesi (CTSI)	Aproximadamente 70 confirmaciones	14 minutos Celer Network (CELR)	Aproximadamente 70 confirmaciones	14 minutos Centrifuge (CFG)	N/A	Casi al instante Chain (XCN)	Aproximadamente 70 confirmaciones	14 minutos Chainlink (LINK)	Aproximadamente 70 confirmaciones	14 minutos Chiliz (CHZ)	Aproximadamente 70 confirmaciones	14 minutos Chromia (CHR)	Aproximadamente 70 confirmaciones	14 minutos Civic (CVC)	Aproximadamente 70 confirmaciones	14 minutos COTI (COTI)	Aproximadamente 70 confirmaciones	14 minutos Compound (COMP)	Aproximadamente 70 confirmaciones	14 minutos Coin98 (C98)	Aproximadamente 70 confirmaciones	14 minutos Convex Finance (CVX)	20 confirmaciones	5 minutos Cosmos (ATOM)	N/A	Casi al instante Covalent (CQT)	Aproximadamente 70 confirmaciones	14 minutos Crust Shadow (CSM)	N/A	Casi al instante Curve (CRV)	Aproximadamente 70 confirmaciones	14 minutos Dai (DAI) ERC-20	Aproximadamente 70 confirmaciones	14 minutos Dash (DASH)	2 confirmaciones	5 minutos Decentraland (MANA)	Aproximadamente 70 confirmaciones	14 minutos Dent (DENT)	Aproximadamente 70 confirmaciones	14 minutos Dogecoin (DOGE)	40 confirmaciones	40 minutos dYdX (DYDX)	Aproximadamente 70 confirmaciones	14 minutos Elrond (EGLD)	1 confirmación	Casi al instante Energy Web Token (EWT)	21 confirmaciones	105 segundos Enjin Coin (ENJ)	Aproximadamente 70 confirmaciones	14 minutos Enzyme Finance (MLN)	Aproximadamente 70 confirmaciones	14 minutos EOS (EOS)	N/A	Casi al instante EthereumPoW (ETHW)**	20.000 confirmaciones	aprox. 3 o 4 días Ethereum (ETH)	Aproximadamente 70 confirmaciones	14 minutos Ethereum Classic (ETC)	2.880 confirmaciones	12 horas Servicio de nombres de Ethereum (ENS)	Aproximadamente 70 confirmaciones	14 minutos Euler (EUL)	Aproximadamente 70 confirmaciones	14 minutos Fantom (FTM)	1 confirmación	Casi al instante Fetch.ai (FET)	N/A	Casi al instante Filecoin (FIL)	200 confirmaciones	100 minutos Flare (FLR)	60 confirmaciones	~2 minutos Flow (FLOW)	30 confirmaciones	1 minuto Frax Share (FXS)	Aproximadamente 70 confirmaciones	14 minutos Gala Games (GALA)	Aproximadamente 70 confirmaciones	14 minutos Galxe (GAL)	Aproximadamente 70 confirmaciones	14 minutos Gari Network (GARI)	N/A	Casi al instante GensoKishi Metaverse (MV)	Aproximadamente 70 confirmaciones	14 minutos Gitcoin (GTC)	Aproximadamente 70 confirmaciones	14 minutos Gnosis (GNO)	Aproximadamente 70 confirmaciones	14 minutos Green Satoshi Token (GST)	N/A	Casi al instante Harvest Finance (FARM)	Aproximadamente 70 confirmaciones	14 minutos Hashflow (HFT)	Aproximadamente 70 confirmaciones	14 minutos ICON (ICX)	3 confirmaciones	Casi al instante IDEX (IDEX)	Aproximadamente 70 confirmaciones	14 minutos iExec (RLC)	Aproximadamente 70 confirmaciones	14 minutos Immutable X (IMX)	Aproximadamente 70 confirmaciones	14 minutos Injective Protocol (INJ)	Aproximadamente 70 confirmaciones	14 minutos Integritee (TEER)	N/A	Casi al instante Interlay (INTR)	N/A	Casi al instante Internet Computer (ICP)	1 confirmación	Casi al instante Jasmy (JASMY)	Aproximadamente 70 confirmaciones 14 minutos JUNO (JUNO)	N/A Casi al instante Karura (KAR)	N/A	Casi al instante Kava (KAVA)	N/A	Casi al instante KeeperDAO (ROOK)	Aproximadamente 70 confirmaciones	14 minutos Keep Network (KEEP)	Aproximadamente 70 confirmaciones	14 minutos Keep3r Network (KP3R)	25 confirmaciones	5 minutos KILT (KILT)	N/A	Casi al instante Kin (KIN)	N/A	Casi al instante Kintsugi (KINT)	N/A	Casi al instante Kusama (KSM)	N/A	Casi al instante Kyber Network (KNC)	Aproximadamente 70 confirmaciones	14 minutos Lido DAO (LDO)	Aproximadamente 70 confirmaciones	14 minutos Liechtenstein Cryptoassets Exchange (LCX)	Aproximadamente 70 confirmaciones	14 minutos Lisk (LSK)	303 confirmaciones	51 minutos Litecoin (LTC)	12 confirmaciones	30 minutos Livepeer (LPT)	Aproximadamente 70 confirmaciones	14 minutos Loopring (LRC)	Aproximadamente 70 confirmaciones	14 minutos Maker (MKR)	Aproximadamente 70 confirmaciones	14 minutos Mango (MNGO)	N/A	Casi al instante Marlin (POND)	Aproximadamente 70 confirmaciones	14 minutos Marinade SOL (MSOL)	N/A	Casi al instante Mask Network (MASK)	Aproximadamente 70 confirmaciones 14 minutos Merit Circle (MC)	Aproximadamente 70 confirmaciones	14 minutos Mina (MINA)	15 confirmaciones	60 minutos Mirror Protocol (MIR)	Aproximadamente 70 confirmaciones	14 minutos Monero (XMR)	15 confirmaciones	30 minutos Moonbeam (GLMR)	N/A	Casi al instante Moonriver (MOVR)	N/A	Casi al instante Multichain (MULTI)	Aproximadamente 70 confirmaciones	14 minutos MXC (MXC)	Aproximadamente 70 confirmaciones	14 minutos My Neighbor Alice (ALICE)	Aproximadamente 70 confirmaciones	5 minutos Nano (NANO)	N/A	Casi al instante Near Protocol (NEAR)	102 bloques	Menos de 2 minutos Nodle (NODL)	N/A	Casi al instante Numeraire (NMR)	Aproximadamente 70 confirmaciones	14 minutos Nym (NYM)	N/A	Casi al instante Ocean (OCEAN)	18 confirmaciones	5 minutos OMG Network (OMG)	Aproximadamente 70 confirmaciones	14 minutos Orca (ORCA)	N/A	Casi al instante Orchid (OXT)	Aproximadamente 70 confirmaciones	14 minutos Origin Protocol (OGN)	Aproximadamente 70 confirmaciones	14 minutos Oxygen (OXY)	N/A	Casi al instante Parallel Finance (PARA)	N/A	Casi al instante PAX Gold (PAXG)	Aproximadamente 70 confirmaciones	14 minutos Perpetual Protocol (PERP)	Aproximadamente 70 confirmaciones	14 minutos Phala (PHA)	N/A	Casi al instante PlayDapp (PLA)	Aproximadamente 70 confirmaciones	14 minutos Polkadot (DOT)	N/A	Casi al instante Polkastarter (POLS)	Aproximadamente 70 confirmaciones	14 minutos Polygon (MATIC), (ERC-20)	Aproximadamente 70 confirmaciones	14 minutos Polygon (MATIC), (red de Polygon)	450 confirmaciones	~9 minutos Powerledger (POWR)	Aproximadamente 70 confirmaciones	14 minutos pSTAKE	Aproximadamente 70 confirmaciones	14 minutos Qtum (QTUM)	100 confirmaciones	60 minutos Quant (QNT)	Aproximadamente 70 confirmaciones	14 minutos Radicle (RAD)	Aproximadamente 70 confirmaciones	14 minutos Rarible (RARI)	Aproximadamente 70 confirmaciones	14 minutos Raydium (RAY)	N/A	Casi al instante Render (RNDR)	Aproximadamente 70 confirmaciones 14 minutos REN Protocol (REN)	Aproximadamente 70 confirmaciones	14 minutos Request (REQ)	Aproximadamente 70 confirmaciones	14 minutos Ripple (XRP)	N/A	Casi al instante Robonomics (XRT)	N/A	Casi al instante Rocket Pool (RPL)	Aproximadamente 70 confirmaciones	14 minutos Rubic (RBC)	Aproximadamente 70 confirmaciones	14 minutos Saber (SBR)	N/A	Casi al instante Samoyed Coin (SAMO)	N/A	Casi al instante Secret (SCRT)	1 confirmación	Casi al instante SelfKey (KEY)	Aproximadamente 70 confirmaciones	14 minutos Serum (SRM)	N/A	Casi al instante Shiba Inu (SHIB)	Aproximadamente 70 confirmaciones	14 minutos Shiden (SDN)	N/A	Casi al instante Siacoin (SC)	6 confirmaciones	60 minutos Solana (SOL)	N/A	Casi al instante Songbird (SGB)	60 confirmaciones	5 minutos Spell Token (SPELL)	Aproximadamente 70 confirmaciones 14 minutos Stacks (STX)	4 confirmaciones EST. 40 minutos StaFi Protocol (FIS)	Aproximadamente 70 confirmaciones 14 minutos Star Atlas (ATLAS)	N/A	Casi al instante Star Atlas DAO (POLIS)	 N/A 	Casi al instante Stargate Finance (STG)	Aproximadamente 70 confirmaciones 14 minutos Stellar Lumens (XLM)	N/A	Casi al instante Step Finance (STEP)	N/A	Casi al instante STEPN (GMT)	N/A	Casi al instante Storj (STORJ)	Aproximadamente 70 confirmaciones	14 minutos SuperFarm (SUPER)	Aproximadamente 70 confirmaciones	14 minutos SuperRare (RARE)	Aproximadamente 70 confirmaciones	14 minutos Sushi (SUSHI)	Aproximadamente 70 confirmaciones	14 minutos Swipe (SXP)	Aproximadamente 70 confirmaciones	14 minutos Synapse (SYN)	Aproximadamente 70 confirmaciones	14 minutos Synthetix (SNX)	Aproximadamente 70 confirmaciones	14 minutos tBTC (TBTC)	Aproximadamente 70 confirmaciones	14 minutos Terra 2.0 (LUNA2)	N/A	Casi al instante Terra Classic (LUNA)	N/A	Casi al instante TerraUSD Classic (UST)	1 confirmación	Casi al instante Terra Virtual Kolect (TVK)	Aproximadamente 70 confirmaciones	14 minutos Tether USD (USDT) ERC-20	Aproximadamente 70 confirmaciones	14 minutos Tether USD (USDT) TRC-20	20 confirmaciones	2 minutos Tezos (XTZ)	6 confirmaciones	3 minutos The Graph (GRT)	Aproximadamente 70 confirmaciones 14 minutos The Sandbox (SAND)	Aproximadamente 70 confirmaciones 14 minutos Thorchain (RUNE)	1 confirmación Casi al instante Threshold (T)	Aproximadamente 70 confirmaciones 14 minutos Tokemak (TOKE)	Aproximadamente 70 confirmaciones 14 minutos Tribe (TRIBE)	Aproximadamente 70 confirmaciones 14 minutos Tron (TRX)	20 confirmaciones	1 minuto TrueFi (TRU)	Aproximadamente 70 confirmaciones	14 minutos Uniswap (UNI)	Aproximadamente 70 confirmaciones	14 minutos Unifi Protocol DAO (UNFI)	Aproximadamente 70 confirmaciones	14 minutos Universal Market Access (UMA)	Aproximadamente 70 confirmaciones 14 minutos USD Coin (USDC) ERC-20	Aproximadamente 70 confirmaciones	14 minutos USD Coin (USDC) SPL	N/A	Casi al instante USD Coin (USDC) TRC-20	20 confirmaciones	1 minuto Waves (WAVES)	10 confirmaciones	10 minutos Woo Network (WOO)	Aproximadamente 70 confirmaciones	14 minutos Wrapped Bitcoin (WBTC)	Aproximadamente 70 confirmaciones	14 minutos Wrapped Ether (WETH)	Aproximadamente 70 confirmaciones	14 minutos Wrapped Axelar (WAXL)	Aproximadamente 70 confirmaciones	14 minutos Yearn Finance (YFI)	Aproximadamente 70 confirmaciones	14 minutos Yield Guild Games (YGG)	Aproximadamente 70 confirmaciones	14 minutos Zcash (ZEC)	24 confirmaciones	60 minutos'
+#data = data.replace('\t', ' ')
+
+
+def replace(frases):
+    global data
+    for frase in frases:
+        data = data.replace(frase, f'{frase}\n')
+
+
+def getName(name):
+    name = name.strip()
+    for i, letra in enumerate(name):
+        if letra == '(':
+            name = name[i+1:-1]
+    return name
+
+
+def getTime(time):
+    time = time.split('confirmación')
+    time = time[0] if len(time) == 1 else time[1]
+    time = time.split('confirmaciones')
+    time = time[0] if len(time) == 1 else time[1]
+    time = time.split('N/A')
+    time = time[0] if len(time) == 1 else time[1]
+    time = time.strip()
+    if time == 'Casi al instante':
+        return 0
+    n = ''
+    tipo = time.split(' ')
+    if tipo[-1] == 'minutos' or tipo[-1] == 'minuto':
+        tipo = 1
+    elif tipo[-1] == 'segundos' or tipo[-1] == 'segundo':
+        tipo = 0
+    elif tipo[-1] == 'horas' or tipo[-1] == 'hora':
+        tipo = 2
+    elif tipo[-1] == 'días':
+        tipo = 3
+    for letra in time:
+        if letra.isdigit():
+            n += letra
+    if n == '':
+        return '-'
+    return int(n)*(60**tipo)
+
+
+frases = ['14 minutos', 'Casi al instante', '5 minutos', '45 segundos', '2 minutos', '1 minuto', '30 minutos', '60 minutos', '51 minutos', '~9 minutos', '3 minutos', '40 minutos', '150 minutos', '10 minutos', '105 segundos', 'aprox. 3 o 4 días', '12 horas', '~2 minutos']
+replace(frases)
+data = data.split('\n')
+data = set(data)
+data.remove('')
+data = list(data)
+for i, coin in enumerate(data):
+    if coin == '':
+        continue
+    aux = coin.split('\t')
+    name = getName(aux[0])
+    time = getTime(aux[2] if len(aux) == 3 else aux[1])
+    data[i] = {'name': name, 'time': time}
+
+diccionario = dict()
+
+for coin in data:
+    diccionario.update({coin['name']: coin['time']})
+print(diccionario)
+#print(len(diccionario))
+#print(data)
+
+
+
